8d3be6a7714b17ed21c9e86aec3050ae961c616c
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-07-15 19:01:54 +0100

    bot

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from "express";
-import "dotenv/config.js";
+import "dotenv/config";
 import rwClient from "./twitterClient.js";
 import { CronJob } from "cron";
 
 const app = express();
 const PORT = 8888;
 
+const tweet = async ()=>{
+    try{
+        await rwClient.v2.tweet(
+            ",Goodevening hope your day is productive! testing server :)"
+        );
+        console.log("tweeted!");
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//time you want it to show 
+//min,hour,day,month,year
+const job = new CronJob("51 18 * * *",()=>{
+    tweet();
+})
+
+
 app.listen(PORT, () => {
   console.log(`server is running http://localhost:${PORT}`);
 });
+
+job.start();
--- twitterClient.js
+++ twitterClient.js
@@ -3,7 +3,7 @@ const client = new TwitterApi({
     appKey: process.env.API_KEY,
     appSecret: process.env.API_SECRET,
     accessToken: process.env.ACCESS_TOKEN,
-    accessSecret: process.env.ACCESS_SECRET,
+    accessSecret: process.env.ACCESS_TOKEN_SECRET,
 })
 
 const rwClient = client.readWrite
